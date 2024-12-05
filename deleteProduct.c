a786b625e3ed343d2badb54f7cdd41d045aab491
jotaa14 <122279638+jotaa14@users.noreply.github.com>
2024-12-05 23:47:42 +0000

    Update deleteProduct.c

--- deleteProduct.c
+++ deleteProduct.c
@@ -13,10 +13,9 @@ void DeleteProduct(Management *management) {
         
         free(ACCESS_TO_PRODUCT_BY_ID);
 
-        //TODO: RESOLVER DELETE PROCESSES
-        /*for(int j = 0; j<management->product[management->product_counter]->process_counter; j++){
-            free(management->product[management->product_counter]->process[j]);
-        }*/ 
+        for(int j = 0; j<management->product[id]->process_counter; j++){
+            free(management->product[id]->process[j]);
+        }
         
         for (int i = id; i < ACCESS_TO_PRODUCT_COUNTER - 1; i++) {
             ACCESS_TO_PRODUCT_BY_I = management->product[i + 1];
@@ -32,32 +31,3 @@ void DeleteProduct(Management *management) {
         puts(ERROR_DELETE_STATUS);
     }
 }
-
-
-
-void CleanDataProduct(Product *product) {
-    ACCESS_PRODUCT_ID=0;
-    strcpy(ACCESS_PRODUCT_NAME, "");
-    ACCESS_PRODUCT_CATEGORY = 0;
-    product->total_hours = 0;
-    product->total_minutes = 0;
-    product->total_seconds = 0;
-    product->process_counter = 0;
-    product->used_by_order = 0;
-    product->searched = 0;
-    product->pvp = 0;
-    
-    strcpy(ACCESS_PRODUCT_STATUS, "");
-}
-
-void CleanDataProcess(Process *process){
-    process->id=0;
-    process->machine = 0;
-    process->process_hours = 0;
-    process->process_minutes = 0;
-    process->process_seconds = 0;
-    strcpy(process->report, "");
-    strcpy(process->status, "");
-           
-}
-    
