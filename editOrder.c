f24d5650ddcd4d2b52e1ba69821cdf9d4e78fb62
jotaa14 <122279638+jotaa14@users.noreply.github.com>
2024-12-06 20:47:44 +0000

    Update editOrder.c

--- editOrder.c
+++ editOrder.c
@@ -3,6 +3,9 @@
 void EditOrder (Management *management){
     int menu;
     ListOrders(management);
+    if(management->order_counter==0){
+        return;
+    }
     int id = GetOrderPosition(*management, GetInt(MIN_SIZE, ORDERS_SIZE, MSG_GET_ORDER));
     
     if(id==-1){
@@ -16,31 +19,29 @@ void EditOrder (Management *management){
         ClearInputBuffer();
         switch(menu){
             case 1:
-                management->order[id]->registration_day = GetInt(MIN_DATE_DAY, MAX_DATE_DAY, GET_ORDER_DATE_DAY);
-                break;
-            case 2:
-                management->order[id]->registration_month = GetInt(MIN_DATE_MONTH, MAX_DATE_MONTH, GET_ORDER_DATE_MONTH);
-                break;
-            case 3:
-                management->order[id]->registration_year = GetInt(MIN_DATE_YEAR, MAX_DATE_YEAR, GET_ORDER_DATE_YEAR);
-                break;
-            case 4:
                 puts(EDIT_ORDER_GET_PRIORITY);
                 int priority = OrderPriorityMenu();
                 management->order[id]->priority = (Priority)priority;
                 break;
-            case 5:
+            case 2:
+                int statusorder;
                 puts(EDIT_ORDER_GET_STATUS);
-                int statusorder = OrderStatusMenu();
-                management->order[id]->statusorder = (StatusOrder)statusorder;
+                do{
+                    statusorder = OrderStatusMenu();
+                    management->order[id]->statusorder = (StatusOrder)statusorder;
+                    if(statusorder==3 && management->order[id]->fulfillment>0){
+                        puts(ERROR_STATUS_CANCELED_CANNOT_BE_SELECTED);
+                        statusorder=-1;
+                    }
+                }while(statusorder=-1);
                 break;
-            case 6:
+            case 3:
                 management->order[id]->fulfillment = GetInt(MIN_FULFILLMENT_VALUE, MAX_FULFILLMENT_VALUE, GET_ORDER_FULFILLMENT_VALUE);
                 break;
-            case 7:
+            case 4:
                 
                 break;
-            case 8:
+            case 5:
                 
                 break;
             case 0:
