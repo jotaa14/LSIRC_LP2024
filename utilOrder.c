7f0e13384864c79ee0156bd43fa30f17f1a89d2e
jotaa14 <122279638+jotaa14@users.noreply.github.com>
2024-12-07 02:25:51 +0000

    Update utilOrder.c

--- utilOrder.c
+++ utilOrder.c
@@ -10,3 +10,16 @@ int GetOrderPosition(Management management, int number){
     return -1;
 }
 
+void AssociateStatusToFulfillment (Management *management, int id){
+    int statusorder;
+    if(management->order[id]->fulfillment == 0){
+    statusorder = 0;
+    management->order[id]->statusorder = (StatusOrder) statusorder;
+    } else if(management->order[id]->fulfillment > 0 && management->order[id]->fulfillment < 100){
+    statusorder = 1;
+    management->order[id]->statusorder = (StatusOrder) statusorder;    
+    } else if(management->order[id]->fulfillment == 100){
+    statusorder = 2;
+    management->order[id]->statusorder = (StatusOrder) statusorder;    
+    }  
+}
