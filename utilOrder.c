4f27f0d84adef89a3a1ed721631ac97682552fe1
jotaa14 <122279638+jotaa14@users.noreply.github.com>
2024-12-09 21:29:24 +0000

    Update utilOrder.c

--- utilOrder.c
+++ utilOrder.c
@@ -10,6 +10,16 @@ int GetOrderPosition(Management management, int number){
     return -1;
 }
 
+int ChooseOrder(Management *management){
+    ListOrders(management);
+    int order_choice = GetInt(MIN_SIZE, ORDERS_SIZE, MSG_GET_ORDER);
+    if(order_choice < 0 || order_choice > management->order_counter){
+        puts(ERROR_NO_ORDERS);
+        return -1;
+    }
+    return order_choice-1;
+}
+
 void AssociateStatusToFulfillment (Management *management, int id){
     int statusorder;
     if(management->order[id]->fulfillment == 0){
@@ -22,4 +32,25 @@ void AssociateStatusToFulfillment (Management *management, int id){
     statusorder = 2;
     management->order[id]->statusorder = (StatusOrder) statusorder;    
     }  
+}
+
+void UpdateTotalPurchasedProducts(Management *management, int id) {
+    if (management->order[id]->statusorder == 3) {
+        Order *order = management->order[id];
+
+        for (int i = 0; i < PRODUCTS_SIZE; i++) {
+            if (order->product[i] != NULL) {
+                Product *product = order->product[i];
+
+                product->total_selled -= order->product_quantity[i];
+
+                if (product->total_selled < 0) {
+                    product->total_selled = 0;
+                }
+
+                order->product[i] = NULL;
+                order->product_quantity[i] = 0;
+            }
+        }
+    }
 }
