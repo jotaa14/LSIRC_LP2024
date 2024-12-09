ca8b1c590c83b1fec66ddd928ef9f7f5607f5e4e
jotaa14 <122279638+jotaa14@users.noreply.github.com>
2024-12-09 21:27:48 +0000

    Update createOrder.c

--- createOrder.c
+++ createOrder.c
@@ -30,7 +30,7 @@ void CreateOrder(Management *management) {
     AssociateStatusToFulfillment (management, management->order_counter);
 
     AssociateCustomerWithOrder(management);
-            
+    AssociateProductWithOrder(management);        
     management->order_counter++;
 }
 
@@ -50,17 +50,83 @@ void AssociateCustomerWithOrder(Management *management) {
 }
 
 void AssociateProductWithOrder(Management *management) {
-    int product_id;
     if (management->product_counter <= 0) {
         puts(ERROR_NO_PRODUCTS);
-        puts(CREATE_NEW_PRODUCTS);
-        CreateProduct(management);
+        return;
     }
 
-    do {
-    product_id = ChooseProduct(management);
-    } while (product_id == -1);
+    Order *current_order = management->order[management->order_counter];
+    int next_product_index = 0;
 
-    //management->order[management->order_counter]->product = management->product[product_id];
-    management->product[product_id]->used_by_order++;
+    for (int i = 0; i < PRODUCTS_SIZE; i++) {
+        if (current_order->product[i] == NULL) {
+            next_product_index = i;
+            break;
+        }
+    }
+
+    int option;
+
+    while (1) {
+        AssociateProductWithOrderOutput();
+        scanf("%d", &option);
+        ClearInputBuffer();
+
+        if (option == 0) {
+            break; 
+        } else if (option == 1) {
+            if (next_product_index >= PRODUCTS_SIZE) {
+                puts(ERROR_MAX_PRODUCTS);
+                break;
+            }
+
+            int product_id = ChooseProduct(management);
+            if (product_id == -1) {
+                continue;
+            }
+
+            bool already_associated = false;
+            for (int i = 0; i < PRODUCTS_SIZE; i++) {
+                if (current_order->product[i] == management->product[product_id]) {
+                    already_associated = true;
+                    break;
+                }
+            }
+
+            if (already_associated) {
+                puts(ERROR_PRODUCT_ALREADY_ASSOCIATED);
+                continue;
+            }
+
+            int quantity;
+            printf(GET_PRODUCT_QUANTITY, management->product[product_id]->name);
+            scanf("%d", &quantity);
+            ClearInputBuffer();
+
+            if (quantity <= 0) {
+                puts(ERROR_INVALID_QUANTITY);
+                continue;
+            }
+
+            current_order->product[next_product_index] = management->product[product_id];
+            current_order->product_quantity[next_product_index] = quantity;
+            management->product[product_id]->total_selled = management->product[product_id]->total_selled + quantity;
+            management->product[product_id]->used_by_order++;
+            current_order->order_pvp = current_order->order_pvp + (management->product[product_id]->pvp*quantity);
+            next_product_index++;
+
+            printf(SUCCESS_ADD_PRODUCT, management->product[product_id]->name);
+            printf(PRINT_QUANTITY, quantity);
+        } else {
+            puts(ERROR_MENU);
+        }
+    }
+
+    if (next_product_index == 0) {
+        puts(ERROR_PRODUCT_NO_ADDED);
+    } else {
+        puts(SECCESS_ADD_PRODUCTS);
+    }
 }
+
+
